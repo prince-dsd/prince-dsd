356efc53a765b49aa07d6b59e2bc3a4351bfb23b
Prince Kumar <neo11prince@gmail.com>
2022-09-11 19:07:09 +0530

    add ga

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from "react-ga";
+
 import Header from './components/Header/Header.component';
 import Main from './components/Main/Main.component';
 import SwitchMode from './components/SwitchMode/SwitchMode.component';
+
 import './App.css';
 
+ReactGA.initialize(process.env.REACT_APP_TRACKING_ID)
+
 function App() {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    },[])
+
     return (
         <div className="App">
             <Header />
